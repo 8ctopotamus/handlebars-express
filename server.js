5b8762bf1699040ed542f671cd611dec77ce6d91
Josh <zylo.codes@gmail.com>
2021-10-23 09:54:08 -0500

    refactor to handlebars

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 const path = require('path')
 const express = require('express')
+const exphbs  = require('express-handlebars')
 
 const PORT = 3001
 const app = express()
 
+app.engine('handlebars', exphbs())
+app.set('view engine', 'handlebars')
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(express.static('public'))
 
 // html routes
-app.get('/about', (req, res) => res.sendFile(path.join(__dirname, 'public', 'about.html')))
-app.get('/contact', (req, res) => res.sendFile(path.join(__dirname, 'public', 'contact.html')))
+app.get('/', (req, res) => res.render('index'))
+
+app.get('/about', (req, res) => res.render('about'))
+
+app.get('/contact', (req, res) => res.render('contact'))
+
+app.get('/services', (req, res) => res.render('services'))
+
+app.get('/blog', (req, res) => res.render('blog'))
 
 app.listen(PORT, () => console.log(`Go to http://localhost:${PORT}`))
